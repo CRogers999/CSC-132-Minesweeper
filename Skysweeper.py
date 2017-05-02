30e12ff68a83be298b8db92765a11f32fbee9365
Zcloutet <zcloutet@gmail.com>
2017-05-02 15:14:25 -0500

    Update Skysweeper.py

--- Skysweeper.py
+++ Skysweeper.py
@@ -8,47 +8,47 @@ class Minesweeper:
         self.tile_plain = PhotoImage(file = "images/tile_plain.gif")
 
 
-        self.tile_no = []
-        for x in range(1, 9):
-            self.tile_no.append(PhotoImage(file = "images/tile_"+str(x)+".gif"))
+        #self.tile_no = []
+        #for x in range(1, 9):
+        #    self.tile_no.append(PhotoImage(file = "images/tile_"+str(x)+".gif"))
 
 
 
-        frame = Frame(master)
+        frame = Frame()
         frame.pack()
 
         self.label1 = Label(frame, text = "Minesweeper")
-        self.label1.grid(row = 0, column = 0, columnspan = 10)
+        self.label1.grid(row = 0, column = 0, columnspan = 8)
 
         self.buttons = {}
-        self.mines = 0
-        x_coord = 1
-        y_coord = 0
+        x_input = 1
+        y_input= 0
         for x in range(0, 64):
             mine = 0
             img = self.tile_plain
 
-            
+            #reword 
             # instantiate button, key [x] is called from the previously set dictionary
-            self.buttons[x] = [Button(frame, image = img),mine,0,x,[x_coord, y_coord], 0]
+            self.buttons[x] = [Button(frame, image = img),mine,0,x,[x_input, y_input], 0]
                                                         
             # calculate coords:
-            y_coord += 1
+            y_input += 1
             
-            if y_coord == 8:
-                y_coord = 0
-                x_coord += 1
+            if y_input == 8:
+                y_input = 0
+                x_input += 1
 
-            print str(x_coord)+ "," +str(y_coord)
+            print str(x_input)+ "," +str(y_input)
             
         # lay buttons in grid
         for x in self.buttons:
             self.buttons[x][0].grid( row = self.buttons[x][4][0], column = self.buttons[x][4][1] )
 
 
+    #need to add mines outside of init i think, or else the class would have to be repeatedly instantiated
+    def addMines():
 
-
-
+    
 def main():
     global game
     #create Tk widget
